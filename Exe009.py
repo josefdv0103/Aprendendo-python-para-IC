af8c3e73ef5f6f9c6e45d03ab9f4811c8d91cc1a
José Augusto <josefdv0103@hotmail.com>
2021-08-15 22:03:23 -0300

    15-08

--- Exe009.py
+++ Exe009.py
@@ -20,33 +20,100 @@
 #    t += 1
 #print('Acertou com {} tentativas. Parabés!'.format(t))
 
-import time
-n = ''
-while n != 5:
-    if n not in (1, 2, 3, 5):
-        a = int(input('Primeiro valor: '))
-        b = int(input('Segundo valor: '))
-    print('''[ 1 ] somar
-[ 2 ] multiplicar 
-[ 3 ] maior
-[ 4 ] novos números
-[ 5 ] sair do programa''')
-    n = int(input('Qual a sua opção? '))
-    if n == 1:
-        print('A soma entre {} + {} é {}'.format(a, b, a+b))
-    elif n == 2:
-        print('A multiplicação entre {} e {} é {}'.format(a, b, a*b))
-    elif n == 3:
-        if a - b > 0:
-            print('Entre {} e {} o maior valor é {}'.format(a, b, a))
-        if a - b < 0:
-            print('Entre {} e {} o maior valor é {}'.format(a, b, b))
-    elif n == 4:
-        print('Informe os numeros novamente: ')
-    elif n == 5:
-        print('Finalizando...')
+#import time
+#n = 4
+#while n != 5:
+#   if n == 4:
+#      a = int(input('Primeiro valor: '))
+#        b = int(input('Segundo valor: '))
+#    print('''[ 1 ] somar
+#[ 2 ] multiplicar 
+#[ 3 ] maior
+#[ 4 ] novos números
+#[ 5 ] sair do programa''')
+#    n = int(input('Qual a sua opção? '))
+#    if n == 1:
+#        print('A soma entre {} + {} é {}'.format(a, b, a+b))
+#    elif n == 2:
+#        print('A multiplicação entre {} e {} é {}'.format(a, b, a*b))
+#    elif n == 3:
+#        if a - b > 0:
+#            print('Entre {} e {} o maior valor é {}'.format(a, b, a))
+#        if a - b < 0:
+#            print('Entre {} e {} o maior valor é {}'.format(a, b, b))
+#    elif n == 4:
+#        print('Informe os numeros novamente: ')
+#    elif n == 5:
+#        print('Finalizando...')
+#    else:
+#        print('Opção inválida. Tente novamente')
+#    print(20*'=-')
+#time.sleep(2)
+#print('Fim do programa! Volte sempre')
+
+# = int(input('Digite um número para calcular o fatorial: '))
+#p = n
+#f = 1
+#print('Calculando {}! = '.format(n), end='')
+#while p > 0:
+#    print('{}'.format(p), end='')
+#    print(' x ' if p > 1 else ' = ', end='')
+#    f *= p
+#    p -= 1
+#print('{}'.format(f))
+
+# 10 primeiros termos de uma PA
+#print(20*'-=', 'Gerador de PA', 20*'=-')
+#p = int(input('Primeiro termo:'))
+#r = int(input('Razão: '))
+#x = 10
+#s = p
+#while x >= 1:
+#    print('{} -> '.format(s), end='')
+#    s += r
+#    x -= 1
+#print('FIM')
+
+# Termos de uma PA (quantos eu quiser)
+#print(20*'=-','Gerador de PA', 20*'=-')
+#p = int(input('Primeiro termo: '))
+#r = int(input('Razão: '))
+#s = p
+#x = 10
+#y = 10
+#z = 0
+#while y != 0:
+#    x = y
+#    while x > 0:
+#        print('{} -> '.format(s), end='')
+#        s += r
+#        x -= 1
+#    print('PAUSA')
+#    y = int(input(('quantos termos vcê quer mostar a mais?')))
+#    z = z + y
+#print('Progressão finalizada com {} termos mostrados'.format(z + 10))
+
+#Fibonacci
+print(20*'-=', '\nSequência de Fibonacci\n', 20*'-=')
+q = int(input(('Quantos termos você quer mostrar?')))
+n = 0
+x = 0
+f1 = 0
+f2 = 1
+while x <= q:
+    if x == 0:
+        print('{} -> '.format(f1+f2), end='')
+    elif x == 1: 
+        print('{} -> '.format(f1+f2), end='')
+    elif x == 2:
+        f1 += 1
+        print('{} -> '.format(f1+f2), end='')
+    elif x == 3:
+        f2 += 1
+        print('{} -> '.format(f1+f2), end='')
     else:
-        print('Opção inválida. Tente novamente')
-    print(20*'=-')
-time.sleep(2)
-print('Fim do programa! Volte sempre')
+        f1 += f1
+        f2 += f2
+        print('{} -> '.format(f1+f2), end='')
+    x += 1
+print('FIM')
